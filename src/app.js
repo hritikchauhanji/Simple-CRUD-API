48b7488d225da2664c9c19b1d578250ae62681e6
Hritik Chauhan <hritikchji@gmail.com>
2026-01-21 08:56:03 +0530

    fix stable version issue

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import Fastify from "fastify";
+import prismaPlugin from "./config/prisma.js";
 
 const app = Fastify({
   logger: true,
 });
+
+app.register(prismaPlugin);
 
 app.get("/health", async () => {
   return {
